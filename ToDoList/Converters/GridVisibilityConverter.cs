7a8ac9ea800da4fe71739e851e96c5758f77b0e4
javohirtohirovich <javoergashev30@gmail.com>
2024-08-02 20:14:07 +0500

    Change MAIN Page UI

---
+++ ToDoList/Converters/GridVisibilityConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace ToDoList.Converters;
+
+public class GridVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is DateTime dueDate)
+        {
+            return dueDate.Date < DateTime.Now.Date;
+        }
+        return false;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
